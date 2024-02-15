c651e4f4bdd5a4d2aeb24f7982584e5f41dfc225
TanvirSEF <160090846+TanvirSEF@users.noreply.github.com>
2024-02-15 09:31:17 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,105 @@
+// condition
+
+// var age = 21
+
+// if(age < 23){
+//     console.log("amra biye korte parbo");
+// }else{
+//     console.log("amra biye korte parbo na");
+// }
+// dom = document object model
+
+// 2 X 1 = 2
+// 2 X 2 = 4
+// 2 X 3 = 6
+// 2 X 4 = 8
+// 2 X 5 = 10
+// 2 X 6 = 12
+// 2 X 7 = 14
+// 2 X 8 = 16
+// 2 X 9 = 18
+// 2 X 10 = 20
+
+
+// let input = document.querySelector(".one")
+// let btn = document.querySelector(".btn")
+// let err = document.querySelector(".err")
+
+// btn.addEventListener("click", function(){
+
+//     if(input.value){
+//         for(i= 1; i <= 10; i++){
+//             document.write(input.value + " X " + i + " = " + input.value * i + "<br>");
+//         }
+//     }else{
+//         err.innerHTML ="Please Some Input Value"
+//     }
+   
+// })
+
+
+
+// function abc(){
+//     console.log("ami");
+// } 
+
+// let ami = function(){
+//     console.log("ami achi");
+// }
+// ami()
+//  =()=>{
+
+//  }
+
+// let arrr = [20,10,"shsh",12,]
+
+// arrr.map((item)=>{
+//     console.log(item);
+// })
+
+// let sum = 0
+
+// function abc(...b){
+    
+//     b.map((item)=>{
+//         sum += item
+//     })
+  
+// }
+// abc(20,39,10,43,451,13234,12)
+// console.log(sum);
+
+
+let students = [
+    {
+        name:"siam",
+        age:34,
+        roll:420
+    },
+    {
+        name:"riad",
+        age:42,
+        roll:420
+    },
+]
+
+students.map((item,id)=>{
+    console.log(item);
+   
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
